c6854a25c6dce4faa913525090e43d067cc2ae7e
Melonde-Zeus <vervoorteddy@outlook.com>
2022-07-20 10:57:11 +0200

    add Accets + created all Objet

---
+++ script.js
@@ -0,0 +1,105 @@
+const collection = [
+    {
+        name:"Venusaur",
+        genre:["Grass","Poison"],
+        weakness:["Fire","Psychis-c","Flying","Ice"],
+        evolution:"Evolution de Ivysaur",
+        image:"./accets/003.png",
+        imageSousEvo :"./accets/002.png",
+        descrition:"ts plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight. ",
+        link:"https://www.pokemon.com/us/pokedex/venusaur"
+    },
+    {
+        name:"Charizard",
+        genre:["Fire","Flying"],
+        Weakness:["Water","Electric","Rock"] ,
+        evolution:"Evolution de Chameleon",
+        image:"./accets/006.png",
+        imageSousEvo :"./accets/005.png",
+        descrition:"It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.",
+        link:"https://www.pokemon.com/us/pokedex/charizard"
+    },
+    {
+        name:"Blastoise",
+        genre:"Water",
+        weakness:["Grass","Electric"],
+        evolution:"Evolution de Wartortle",
+        image:"./accets/009.png",
+        imageSousEvo :"./accets/008.png",
+        descrition:"It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell. ",
+        link:"https://www.pokemon.com/us/pokedex/blastoise"
+    },
+    {
+        name:"Butterfree",
+        genre:["Bug","Flying"],
+        weakness:["Fire","Flying","Electric","Ice","Rock"],
+        evolution:"Evolution de Metapod",
+        image:"./accets/012.png",
+        imageSousEvo :"./accets/011.png",
+        descrition:" In battle, it flaps its wings at great speed to release highly toxic dust into the air.",
+        link:"https://www.pokemon.com/us/pokedex/butterfree"
+    },
+    {
+        name:"Beedrill",
+        genre:["Bug","Poison"],
+        weakness:["Fire","Psychic","Flying","Rock"],
+        evolution:"Evolution de Kakuna",
+        image:"./accets/015.png",
+        imageSousEvo :"./accets/014.png",
+        descrition:"It has three poisonous stingers on its forelegs and its tail. They are used to jab its enemy repeatedly.",
+        link:"https://www.pokemon.com/us/pokedex/beedrill"
+    },
+    {
+        name:"Pidgeot",
+        genre:["Normal","Flying"],
+        weakness:["Electric","Ice","Rock"],
+        evolution:"Evolution de Pidgeotte",
+        image:"./accets/018.png",
+        imageSousEvo :"./accets/017.png",
+        descrition:"This Pokémon flies at Mach 2 speed, seeking prey. Its large talons are feared as wicked weapons.",
+        link:"https://www.pokemon.com/us/pokedex/pidgeot"
+    },
+    {
+        name:"Raticate",
+        genre:"Normal",
+        weakness:"Fighting",
+        evolution:"Evolution de Rattata ",
+        image:"./accets/020.png",
+        imageSousEvo :"./accets/019.png",
+        descrition:"Its hind feet are webbed. They act as flippers, so it can swim in rivers and hunt for prey.",
+        link:"https://www.pokemon.com/us/pokedex/raticate"
+    },
+    {
+        name:"Fearow",
+        genre:["Normal","Flying"],
+        weakness:["Electric","Ice","Rock"],
+        evolution:"Evolution de Spearow",
+        image:"./accets/022.png",
+        imageSousEvo :"./accets/021.png",
+        descrition:"A Pokémon that dates back many years. If it senses danger, it flies high and away, instantly.",
+        link:"https://www.pokemon.com/us/pokedex/fearow"
+    },
+    {
+        name:"Arbok",
+        genre:"Poison",
+        weakness:["Psychic","Ground"],
+        evolution:"Evolution de Ekans",
+        image:"./accets/024.png",
+        imageSousEvo :"./accets/023.png",
+        descrition:"The frightening patterns on its belly have been studied. Six variations have been confirmed.",
+        link:"https://www.pokemon.com/us/pokedex/arbok"
+    },
+    {
+        name:"Raichu",
+        genre:"Electric",
+        weakness:"Ground",
+        evolution:"Evolution de Pickachu",
+        image:"./accets/026.png",
+        imageSousEvo :"./accets/025.png",
+        descrition:"Its long tail serves as a ground to protect itself from its own high-voltage power.",
+        link:"https://www.pokemon.com/us/pokedex/raichu"
+    },
+];
+
+
+
